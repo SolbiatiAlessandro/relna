53d242130cdd0330d4d24f68ee0a0e2e4b1d340b
Solbiati Alessandro <alexsolbiati@hotmail.it>
2019-05-27 01:35:19 +0100

    webapp and database communicate succesfuly

--- relna/db.py
+++ relna/db.py
@@ -1,22 +1,26 @@
 import psycopg2
 import os
+db_user = os.environ.get('CLOUD_SQL_USERNAME')
 db_password = os.environ.get('CLOUD_SQL_PASSWORD')
+db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
+db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 
 def query_db(query='SELECT NOW() as now;'):
     """
     query psycopg2 db on relna project
+
+    ###########FAIL TO AUTHENTICATE LOCALLY#######
+    you need to export password
+    export CLOUD_SQL_PASSWORD=""
     """
     if os.environ.get('GAE_ENV') == 'standard':
         # If deployed, use the local socket interface for accessing Cloud SQL
         host = '/cloudsql/{}'.format(db_connection_name)
+        cnx = psycopg2.connect(dbname=db_name, user=db_user,
+                                password=db_password, host=host)
     else:
-        # If running locally, use the TCP connections instead
-        # Set up Cloud SQL Proxy (cloud.google.com/sql/docs/mysql/sql-proxy)
-        # so that your application can use 127.0.0.1:3306 to connect to your
-        # Cloud SQL instance
-        host = '127.0.0.1'
+        cnx = psycopg2.connect("sslmode=disable dbname=postgres user=postgres hostaddr=104.199.68.166 password={}".format(db_password))
 
-    cnx = psycopg2.connect("sslmode=disable dbname=postgres user=postgres hostaddr=104.199.68.166 password={}".format(db_password))
 
     with cnx.cursor() as cursor:
         cursor.execute(query)
